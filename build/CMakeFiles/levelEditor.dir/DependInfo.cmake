beea61c7dbd32282d2625cff60f32c316c7f3e5c
Furkan <furkandutoglu@gmail.com>
2013-11-21 19:58:13 +0200

    Added sdl-image to dependencies file

---
+++ build/CMakeFiles/levelEditor.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "E:/aecs-top-down-game-engine/ai/leftTurner.c" "E:/aecs-top-down-game-engine/build/CMakeFiles/levelEditor.dir/ai/leftTurner.c.obj"
+  "E:/aecs-top-down-game-engine/engine.c" "E:/aecs-top-down-game-engine/build/CMakeFiles/levelEditor.dir/engine.c.obj"
+  "E:/aecs-top-down-game-engine/level-editor.c" "E:/aecs-top-down-game-engine/build/CMakeFiles/levelEditor.dir/level-editor.c.obj"
+  "E:/aecs-top-down-game-engine/map.c" "E:/aecs-top-down-game-engine/build/CMakeFiles/levelEditor.dir/map.c.obj"
+  "E:/aecs-top-down-game-engine/object.c" "E:/aecs-top-down-game-engine/build/CMakeFiles/levelEditor.dir/object.c.obj"
+  "E:/aecs-top-down-game-engine/texture.c" "E:/aecs-top-down-game-engine/build/CMakeFiles/levelEditor.dir/texture.c.obj"
+  "E:/aecs-top-down-game-engine/vector.c" "E:/aecs-top-down-game-engine/build/CMakeFiles/levelEditor.dir/vector.c.obj"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../levelEditor"
+  "../libs/SDL/include/SDL2"
+  "../libs/SDL_image/include/SDL2"
+  "../aec-game"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/levelEditor.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/levelEditor.dir/level-editor.c.obj"
+  "CMakeFiles/levelEditor.dir/engine.c.obj"
+  "CMakeFiles/levelEditor.dir/texture.c.obj"
+  "CMakeFiles/levelEditor.dir/map.c.obj"
+  "CMakeFiles/levelEditor.dir/object.c.obj"
+  "CMakeFiles/levelEditor.dir/vector.c.obj"
+  "CMakeFiles/levelEditor.dir/ai/leftTurner.c.obj"
+  "levelEditor.pdb"
+  "levelEditor.exe"
+  "levelEditor.exe.manifest"
+  "liblevelEditor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/levelEditor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
