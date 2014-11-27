830ca8b9ec66cf44eae7d3e0164bddec9dadb9b1
aecepoglu <aecepoglu@fastmail.fm>
2014-11-27 11:31:27 +0200

    Issue #161 - Creating map with walls in edges

--- src/map.c
+++ src/map.c
@@ -143,8 +143,12 @@ struct Map* createNewMap( unsigned int width, unsigned int height) {
 	for(x=0; x<width; x++) {
 		m->tiles[x] = (enum terrainType*)calloc( height, sizeof(enum terrainType));
 		m->objs[x] = (struct object**)calloc( height, sizeof(struct object*));
-		for(y=0; y<height; y++)
-			m->tiles[x][y] = terrain_gnd;
+		for(y=0; y<height; y++) {
+			if( x == 0 || y == 0 || x == width-1 || y == height-1 )
+				m->tiles[x][y] = terrain_wall;
+			else
+				m->tiles[x][y] = terrain_gnd;
+		}
 	}
 
 	log1( "createMap() over\n");
