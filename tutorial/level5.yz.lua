064a3f341bab37e14fc3e34d080085200bf815ac
aecepoglu <aecepoglu@fastmail.fm>
2014-11-28 10:42:55 +0200

    tutorial/level5 switches

--- tutorial/level5.yz.lua
+++ tutorial/level5.yz.lua
@@ -25,31 +25,22 @@ end)
 
 lib.onInteract( SW_1, function()
     switch1_on = not switch1_on
-    printSwitch(1, switch1_on)
-    printSwitch(2, switch2_on)
-    printSwitch(3, switch3_on)
+    lib.use( SW_1)
 end)
 
 lib.onInteract( SW_2, function()
     switch2_on = not switch2_on
-    printSwitch(1, switch1_on)
-    printSwitch(2, switch2_on)
-    printSwitch(3, switch3_on)
+    lib.use( SW_2)
 end)
 
 lib.onInteract( SW_3, function()
     switch3_on = not switch3_on
-    printSwitch(1, switch1_on)
-    printSwitch(2, switch2_on)
-    printSwitch(3, switch3_on)
+    lib.use( SW_3)
 end)
 
 lib.onInteract( END, function()
     if switch1_on or switch2_on or switch3_on then
         lib.write("\nAll of the switches must be on !")
-        printSwitch(1, switch1_on)
-        printSwitch(2, switch2_on)
-        printSwitch(3, switch3_on)
     else
         lib.endLevel(1)
     end
